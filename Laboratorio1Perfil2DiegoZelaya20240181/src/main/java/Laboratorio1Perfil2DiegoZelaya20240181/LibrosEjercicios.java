f68e3ed2838db0330f95d6bfdd4cba860337196c
Zelayita <20240181@ricaldone.edu.sv>
2025-06-03 15:45:08 -0600

    "añadiendo todo lo que tiene mi Tarea"

--- Laboratorio1Perfil2DiegoZelaya20240181/src/main/java/Laboratorio1Perfil2DiegoZelaya20240181/LibrosEjercicios.java
+++ Laboratorio1Perfil2DiegoZelaya20240181/src/main/java/Laboratorio1Perfil2DiegoZelaya20240181/LibrosEjercicios.java
@@ -30,26 +30,26 @@ public class LibrosEjercicios {
                     System.out.println("Bienvenido a Agregar Libros");
                     System.out.println("1. Agregar Libro ");
                     int res = sc.nextInt();
-                        //Añadir al HashSet
-                        System.out.println("Que Libro deseas añadr");
-                        String Libro = sc.next();
-                        boolean respuLibro = LibrosHash.contains(Libro);
-                        if (respuLibro) {
-                            System.out.println("Ese libro ya existe");
-                        } else {
-                            //Añadimos al HashSet
-                            LibrosHash.add(Libro);
-                            //Añadir al TreeMap
-                            System.out.println("El ISBN del Libro");
-                            sc.nextLine();
-                            int ISBN = sc.nextInt();
-                            librosTree.put(ISBN, Libro);
-                            //Agregar al HAshMap
-                            sc.nextLine();
-                            System.out.println("EL Genero del Libro?");
-                            String libroGenero = sc.next();
-                            LibrosMap.put(ISBN, libroGenero);
-                            //Agregarmos a la LinbkedList
+                    //Añadir al HashSet
+                    System.out.println("Que Libro deseas añadr");
+                    String Libro = sc.next();
+                    boolean respuLibro = LibrosHash.contains(Libro);
+                    if (respuLibro) {
+                        System.out.println("Ese libro ya existe");
+                    } else {
+                        //Añadimos al HashSet
+                        LibrosHash.add(Libro);
+                        //Añadir al TreeMap
+                        System.out.println("El ISBN del Libro");
+                        sc.nextLine();
+                        int ISBN = sc.nextInt();
+                        librosTree.put(ISBN, Libro);
+                        //Agregar al HAshMap
+                        sc.nextLine();
+                        System.out.println("EL Genero del Libro?");
+                        String libroGenero = sc.next();
+                        LibrosMap.put(ISBN, libroGenero);
+                        //Agregarmos a la LinbkedList
                             if (LibrosList.size() > 10) {
                                 System.out.println("Se eliminara el registro mas Antiguo");
                                 LibrosList.removeLast();
@@ -60,8 +60,23 @@ public class LibrosEjercicios {
                         }
                     break;
                 case 2:
+                    System.out.println("Buscar Por Genero:");
+                    sc.nextLine();
+                    String Genero = sc.next();
+                    boolean genero = LibrosMap.containsValue(Genero);
+                    if (genero){
+                        System.out.println("La clave " + Genero + " Existe en el HashMap");
+                        System.out.println("Su Valor " + LibrosMap.get(Genero));
+                    }else{
+                        System.out.println("La clave "+ Genero + " No existe en La Libreria");
+                    }
                     break;
                 case 3:
+                    System.out.println("Metodo Obtener Ultimos Libros:");
+                    sc.nextLine();
+                    System.out.println("Ultimos Libros añadidos de Mayor a Menor");
+                    System.out.println("Rango de los libros (Mayor a Menor)");
+                    int NumeroLibro = sc.nextInt();
 
                     break;
                 case 4:
