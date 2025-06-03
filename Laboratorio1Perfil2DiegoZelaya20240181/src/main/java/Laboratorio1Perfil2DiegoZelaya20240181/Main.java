70c0a51512542ffbceb77cd01ebbabd95f49c795
Zelayita <20240181@ricaldone.edu.sv>
2025-06-03 13:57:23 -0600

    "Primer Cambio del Laboratorio"

--- Laboratorio1Perfil2DiegoZelaya20240181/src/main/java/Laboratorio1Perfil2DiegoZelaya20240181/Main.java
+++ Laboratorio1Perfil2DiegoZelaya20240181/src/main/java/Laboratorio1Perfil2DiegoZelaya20240181/Main.java
@@ -1,5 +1,5 @@
 package Laboratorio1Perfil2DiegoZelaya20240181;
-//LAboratori de Diego Alberto López Zelaya.
+//Laboratorio de Diego Alberto López Zelaya.
 public class Main {
     public static void main(String[] args) {
     }
